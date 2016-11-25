1bc4881318f306c384e811db58104a7ae1afeafb
jiangKlijna <leil729564090@outlook.com>
2016-11-25 22:29:54 +0800

    :boom:update gin-gorm

--- gin-gorm/src/com/jiangKlijna/orm/db.go
+++ gin-gorm/src/com/jiangKlijna/orm/db.go
@@ -1,46 +1,15 @@
 package orm
 
 import (
-  "fmt"
-  "database/sql"
-  _ "github.com/bmizerany/pq"
+    "github.com/jinzhu/gorm"
+    // _ "github.com/jinzhu/gorm/dialects/mysql"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
+    // _ "github.com/jinzhu/gorm/dialects/sqlite"
+    // _ "github.com/jinzhu/gorm/dialects/mssql"
 )
 
-func Test()  {
-  fmt.Println("db test")
-}
-
-func Postgres() (*sql.DB, error) {
-  return sql.Open("postgres", "user=postgres dbname=postgres password=74898489 sslmode=verify-full sslmode=disable")
-}
-
-func init() {
-  db, err := Postgres()
-  if err != nil{
-    fmt.Println(err.Error())
-    return
-  }
-  row := db.QueryRow("select * from base_user limit 1")
-  var username string
-  row.Scan(&username)
-  fmt.Println(username)
-  //db.Exec("truncate table t");
-
-  db.Exec("insert into base_user (select * from base_user order by id ASC limit 5)")
-
-  rows, err := db.Query("select id, password, username from base_user limit 10")
-  if err != nil{
-    fmt.Println(err.Error())
-    return
-  }
-  for rows.Next() {
-    var username string
-    var id int
-    var password string
-
-    rows.Scan(&id, &username, &password)
-    fmt.Println(id, username, password)
-  }
-  db.Close()
-
+func DB() (*orm.DB, error) {
+  return gorm.Open("postgres", "host=127.0.0.1 user=postgres dbname=test sslmode=disable password=74898489")
+  // return gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+  // return gorm.Open("sqlite3", "/tmp/gorm.db")
 }
