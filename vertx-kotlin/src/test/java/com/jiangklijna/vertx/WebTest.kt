0aaf9851c69819c2fc29b51067a16b74d01a236b
jiangKlijna <leil729564090@outlook.com>
2018-03-24 17:25:44 +0800

    :fire:add WebTest

--- vertx-kotlin/src/test/java/com/jiangklijna/vertx/WebTest.kt
+++ vertx-kotlin/src/test/java/com/jiangklijna/vertx/WebTest.kt
@@ -1,11 +1,44 @@
 package com.jiangklijna.vertx
 
+import io.vertx.core.impl.VertxFactoryImpl
+import io.vertx.ext.web.client.WebClient
 import org.junit.Test
 
 class WebTest {
 
-	@Test
-	fun main() {
-	}
+    private val vertx = VertxFactoryImpl().vertx()
+    private val server = vertx.createHttpServer()
+    private val client = WebClient.create(vertx)
+
+    private fun serverStart() {
+        server.requestHandler({ request ->
+            val response = request.response()
+            response.putHeader("content-type", "text/html")
+
+            response.end("<h1>Hello vertx!</h1><h2>${request.uri()}</h2>")
+        })
+        server.listen(8080)
+        println("listen 8080")
+    }
+
+    private fun clientRequest() {
+        client[8080, "127.0.0.1", "/"].send { ar ->
+            if (ar.succeeded()) {
+                val response = ar.result()
+                println("Got HTTP response with status " + response.statusCode())
+                println(response.body())
+            } else {
+                ar.cause().printStackTrace()
+            }
+        }
+        client.close()
+    }
+
+    @Test
+    fun run() {
+        serverStart()
+        clientRequest()
+        Thread.sleep(1000)
+    }
 
 }
